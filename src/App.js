ee93fdaed61a889b1de3c460a7ae50cafc90555a
unknown <codename100.alan@gmail.com>
2018-09-01 03:50:58 -0400

    Installed Redux.Added Provider component and created store

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Provider} from 'react-redux';
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(()=>[], {}, applyMiddleware());
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Postform />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Postform />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
